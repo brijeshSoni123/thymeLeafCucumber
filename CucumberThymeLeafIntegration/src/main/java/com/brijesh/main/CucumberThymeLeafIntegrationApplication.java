79af270e635fc18867288dea12def826df3dd1ad
brije <brije@BRIJESH>
2018-03-16 20:13:25 +0530

    initial commit for thymeleaf and spring boot integration

---
+++ CucumberThymeLeafIntegration/src/main/java/com/brijesh/main/CucumberThymeLeafIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.brijesh.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberThymeLeafIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberThymeLeafIntegrationApplication.class, args);
+	}
+}
---
+++ CucumberThymeLeafIntegration/src/test/java/com/brijesh/main/CucumberThymeLeafIntegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brijesh.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CucumberThymeLeafIntegrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
